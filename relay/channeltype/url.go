4b5018b6558c26e82abfda63de540fbefd3cd8db
fangfangOvO <109072954+fangfangOvO@users.noreply.github.com>
2025-04-29 16:18:09 +0800

    Update url.go

--- relay/channeltype/url.go
+++ relay/channeltype/url.go
@@ -35,7 +35,7 @@ var ChannelBaseURLs = []string{
 	"https://api.lingyiwanwu.com",               // 31
 	"https://api.stepfun.com",                   // 32
 	"",                                          // 33
-	"https://api.coze.com",                      // 34
+	"https://api.coze.cn",                      // 34
 	"https://api.cohere.ai",                     // 35
 	"https://api.deepseek.com",                  // 36
 	"https://api.cloudflare.com",                // 37
